8ab0458061d4ee6146e9e60ff4aa21631a906b93
SantiagoMarquez06 <santiago.marquez.06@est.ucacue.edu.ec>
2026-01-13 10:33:37 -0500

    push1

---
+++ src/test/java/com/aws/deploy/aws/deploy/AwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.deploy.aws.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
